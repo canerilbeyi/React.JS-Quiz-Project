14e339dfac897384e8e930f91bf977fed22c7553
Caner ilbeyi <canerilbeyi@gmail.com>
2021-09-20 16:17:17 +0300

    initial commit

---
+++ src/SetupForm.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+const SetupForm = () => {
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
+  return (
+    <main>
+      <section className="quiz quiz-small">
+        <form className="setup-form">
+          <h2>setup quiz</h2>
+          {/* amount */}
+          <div className="form-control">
+            <label htmlFor="amount">number of questions</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              value={quiz.amount}
+              className="form-input"
+              min={1}
+              max={10}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-control">
+            {/* category */}
+            <label htmlFor="category">category</label>
+            <select
+              type="text"
+              name="category"
+              id="category"
+              className="form-input"
+              value={quiz.category}
+              onChange={handleChange}
+            >
+              <option value="sports">sports</option>
+              <option value="history">history</option>
+              <option value="politics">politics</option>
+            </select>
+          </div>
+          <div className="form-control">
+            {/* difficulty */}
+            <label htmlFor="difficulty">difficulty</label>
+            <select
+              type="text"
+              name="difficulty"
+              id="difficulty"
+              className="form-input"
+              value={quiz.difficulty}
+              onChange={handleChange}
+            >
+              <option value="easy">easy</option>
+              <option value="medium">medium</option>
+              <option value="hard">hard</option>
+            </select>
+          </div>
+          {error && (
+            <p className="error">
+              can't generate questions, please try different options
+            </p>
+          )}
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default SetupForm;
---
+++ src/context.js
@@ -0,0 +1,120 @@
+import axios from "axios";
+import React, { useState, useContext, useEffect } from "react";
+
+const table = {
+  sports: 21,
+  history: 23,
+  politics: 24,
+};
+
+const API_ENDPOINT = "https://opentdb.com/api.php?";
+
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: "sports",
+    difficulty: "easy",
+  });
+  const [error, setError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(true);
+    const response = await axios(url).catch((err) => console.log(err));
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setWaiting(false);
+        setLoading(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  const nextQuestion = () => {
+    setIndex((oldIndex) => {
+      const newIndex = oldIndex + 1;
+      if (newIndex > questions.length - 1) {
+        openModal();
+        return 0;
+      } else {
+        return newIndex;
+      }
+    });
+  };
+
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1);
+    }
+    nextQuestion();
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setCorrect(0);
+    setWaiting(true);
+  };
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setQuiz({ ...quiz, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const tempUrl =
+    //   "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+    const { category, amount, difficulty } = quiz;
+    const url = `${API_ENDPOINT}amount=${amount}&category=${table[category]}&difficulty=${difficulty}&type=multiple`;
+    console.log(url);
+    fetchQuestions(url);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+        nextQuestion,
+        checkAnswer,
+        closeModal,
+        quiz,
+        handleChange,
+        handleSubmit,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
